3ac582327148ba13ca33255e0e975069e46ea73c
Guadalupe Bonilla Lopez <guadaboni03@gmail.com>
2024-02-27 01:01:20 -0600

    Octavo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('hola mundo pagina de inicio')
   return (
     <main className={styles.main}>
       <h1>hola mundo</h1>
