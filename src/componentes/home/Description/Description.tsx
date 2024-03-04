6e2c5cb8abae499370ef97f2b7701951bd1dc6a5
Guadalupe Bonilla Lopez <guadaboni03@gmail.com>
2024-03-03 21:47:33 -0600

    2_5 Manejo y optimizacion de imagenes

--- src/componentes/home/Description/Description.tsx
+++ src/componentes/home/Description/Description.tsx
@@ -1,10 +1,20 @@
+import styles from './Description.module.sass'
+import Image from 'next/image';
+
 export const Description = () => {
+    
     return(
-        <section>
-            
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Descripcion </h2>
+        <section className={styles.Description}>
+        <Image 
+        src="/images/description.jpeg"
+        alt="products marketplace"
+        width={500}
+        height={300}
+        />
+        <div className={styles.Description__text}> 
+        <h2> Dios eliminame o iluminame</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione quod officiis rerum id mollitia, veritatis consequuntur nihil sint doloremque soluta quam in incidunt blanditiis maxime rem dicta ea! Dignissimos, dolorum.</p>
+            </div>
         </section>
     )
 }
