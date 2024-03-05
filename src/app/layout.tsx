75a2170e08716007534eb40429a774d84ad08893
Guadalupe Bonilla Lopez <guadaboni03@gmail.com>
2024-03-04 21:41:25 -0600

    2_7 Optimizacion de fuentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 
-import { Inter } from "next/font/google"; 
+import {Roboto} from 'next/font/google'
 import { Header } from "app/componentes/Header";
 import { Footer } from "app/componentes/shared/Footer";
 import 'app/sass/global.sass' 
 
-const inter = Inter({ subsets: ["latin"] });
+
+const roboto = Roboto({
+  weight:["100","300","500","700"],
+  subsets:["latin"],
+})
 
 
 export default function RootLayout({
@@ -16,7 +20,7 @@ export default function RootLayout({
   
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body  className={roboto.className}>
       <Header/>
 
         {children}
