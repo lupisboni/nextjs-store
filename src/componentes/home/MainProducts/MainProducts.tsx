40084ff2788013f7489b918d1ddd520176b2b7a9
lupisboni <guadaboni03@gmail.com>
2024-04-19 23:43:09 -0600

    4_11 Como funciona el fetch y el cache

--- src/componentes/home/MainProducts/MainProducts.tsx
+++ src/componentes/home/MainProducts/MainProducts.tsx
@@ -1,17 +1,17 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
-//import { getProducts } from "app/services/shopify/products"
+import { getProducts } from "app/services/shopify/products"
 
 
 export const MainProducts = async () => {
-  const response = await fetch('http://localhost:3000/api');
-    const {products} = await response.json()
+  const products = await getMainProducts()
+
   
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map((product) => {
+           {products?.map((product) => {
           const imageSrc = product.images[0].src;
           return (
             <article key={product.id}>
--- src/services/shopify/urls.ts
+++ src/services/shopify/urls.ts
@@ -7,6 +7,12 @@ export const shopifyUrls ={
     
     },
     collections: {
+        export const shopifyUrls = {
+            products: {
+              'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`,
+              mainProducts: `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/425365307612/products.json`,
+            },
+            collections: {
         'all': `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/smart_collections.json`,
     'products':(id: string) => `${env.SHOPIFY_HOSTNAME}/admin/api/2023-10/collections/${id}/products.json`
       }
