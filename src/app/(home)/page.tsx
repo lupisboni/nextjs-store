2e67b4ddbf15aeb805e8a154f08e3d15c4232361
lupisboni <guadaboni03@gmail.com>
2024-04-20 00:17:20 -0600

    4_15 Mejorando SEO de una pagina

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,26 @@
 import { Description } from "app/componentes/home/Description"
 import { Hero } from "app/componentes/home/Hero"
 import { MainProducts } from "app/componentes/home/MainProducts"
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Future World 🌟",
+  description: "Welcome to the future world",
+  keywords: ["ecommerce", "future", "world"]
+}
 
+export async function generateMetadata(searchParams: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images:[product.image]
+    }
+  }
+}
 
 export default function Home() {
   
